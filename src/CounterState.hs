50e6e1f9f9f575ac0eff68b2479f9174bf5579da
Marek Fajkus <marek.faj@gmail.com>
2018-08-14 07:49:19 +0100

    solve discconnecting

--- src/CounterState.hs
+++ src/CounterState.hs
@@ -17,14 +17,14 @@ import Control.Concurrent (MVar)
 import Counter (CounterStream, Counter)
 import Control.Monad (forM_, forever)
 import Counter (CounterEvent)
+import Control.Exception (finally)
 import qualified Counter
 import qualified Control.Concurrent as Concurrent
 import qualified Network.WebSockets as WS
-import qualified Data.Text as Text
 import qualified Data.Aeson as Aeson
 
 
-type Client = WS.Connection
+type Client = (Int, WS.Connection)
 
 data ServerState = ServerState
   { clients :: MVar [Client]
@@ -44,18 +44,36 @@ current server =
   Counter.getCurrentState $ counter server
 
 
-addClient :: Client -> [Client] -> [Client]
-addClient client clients = client : clients
+addClient :: WS.Connection -> [Client] -> (Int, [Client])
+addClient conn [] = (0, [(0, conn)])
+addClient conn xs@((prevId,_):_) = (newId, (newId, conn) : xs)
+  where newId = prevId + 1
+
+
+removeClient :: Int -> [Client] -> [Client]
+removeClient connId clients = filter ((/= connId) . fst) clients
 
 
 newConnection :: ServerState -> WS.Connection -> IO ()
 newConnection state conn = do
   counterState <- current state
-  Concurrent.modifyMVar_ (clients state) $ \clients -> do
+  cs <- Concurrent.readMVar $ clients state
+  let (connId, newClients) = addClient conn cs
+
+  -- update MVar
+  Concurrent.modifyMVar_ (clients state) $ const $ pure newClients
+
+  -- Disconnect user at the end of session
+  flip finally (disconnect connId) $ do
+        WS.forkPingThread conn 30
         WS.sendTextData conn (Aeson.encode counterState)
-        pure $ addClient conn clients
-  WS.forkPingThread conn 30
-  handle conn state
+        handle conn state
+
+  where
+    disconnect connId =
+      Concurrent.modifyMVar (clients state) $ \s ->
+                let s' = removeClient connId s
+                in pure (s', s')
 
 
 broadcast :: ServerState -> IO ()
@@ -63,11 +81,11 @@ broadcast state = do
     clients <- Concurrent.readMVar $ clients state
     counter <- current state
     forM_ clients $
-      \conn -> do
+      \(_, conn) -> do
         WS.sendTextData conn $ Aeson.encode counter
 
 
-handle :: Client -> ServerState -> IO ()
+handle :: WS.Connection -> ServerState -> IO ()
 handle conn state = forever $ do
   msg <- WS.receiveData conn
   case Aeson.decode msg of
